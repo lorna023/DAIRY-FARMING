abe158a57cee307013755d18ae8c3a5d37ec04e9
lorna023 <lornakirui023@gmail.com>
2021-03-17 09:37:05 +0300

    index css

--- index.js
+++ index.js
@@ -1,51 +1,40 @@
-var leapYearReport = function(){
-    var months ={
-        January:31,
-        February:29,
-        March:31,
-        April:30,
-        May:31,
-        June:30,
-        July:31,
-        August:31,
-        September:30,
-        October:31,
-        November:30,
-        December:31
-    }
-    monthArray = Object.keys(months)
-    var i=0;
-    for (x in months) {
-       console.log("Your income for " + monthArray[i] + " is " + 84420 * months[x]);
-       i++;
-      }
-    }
-    
-    
-    
-    var incomeOverTime = function(sellingPrice,time){
-        if(time=="weekly"){
-            console.log("Your weekly income will be " + sellingPrice * 7 *1876);
-        }else if(time=="yearly"){
-            console.log("Your yearly income will be " + sellingPrice * 365 * 1876);
-        }
-    }
-    
-    // incomeOverTime(45,"weekly");
-    
-    var shedReports = function(){
-    var display = document.getElementById("display")
-    var sheds ={
-        A:510,
-        B:308,
-        C:486,
-        D:572
-     }
-     shedArray = Object.keys(sheds)
-     var i=0;
-     for (x in sheds) {
-        console.log("Your production in " + shedArray[i] + " is " + sheds[x] + " litres per day");
-        display.innerHTML = "<p> This will display </p>"
-        i++;
-       }
-    }
+function daymilk() {
+    shedA =(document.getElementById("shedA").value);
+    shedB =(document.getElementById("shedB").value);
+    shedC =(document.getElementById("shedC").value);
+    shedD =(document.getElementById("shedD").value);
+    shedA = parseFloat(shedA);
+    shedB = parseFloat(shedB);
+    shedC = parseFloat(shedC);
+    shedD = parseFloat(shedD);
+    milk=shedA+shedB+shedC+shedD;
+    document.getElementById("sheda").innerHTML=shedA;
+    document.getElementById("shedb").innerHTML=shedB;
+    document.getElementById("shedc").innerHTML=shedC;
+    document.getElementById("shedd").innerHTML=shedD;
+    document.getElementById("milk").innerHTML =milk;
+}
+function overtime(){
+ rate=(document.getElementById("rate").value);
+ milk=(document.getElementById("todaymilk").value);
+ rate =parseFloat(rate);
+ milk =parseFloat(milk);
+ week=rate*milk*7;
+ month=rate*milk*30;
+ year=rate*milk*365;
+ document.getElementById("week").innerHTML=week;
+ document.getElementById("month").innerHTML=month;
+ document.getElementById("year").innerHTML=year;
+}
+function wholeyear(){
+totalDayMilk=(document.getElementById("totaldaymilk").value);
+totalDayMilk =parseFloat(totalDayMilk);
+income=totalDayMilk*45;
+months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+days= [ 31,29,31,30,31,30,31,31,30,31,30,31];
+for (i = 0; i<months.length; i++){
+  outcome = days[i] * income;
+  document.getElementById("revenue").innerHTML+=("Your income for " + months[i] +  "is  Ksh  " + outcome + "</p>");
+}
+
+}
